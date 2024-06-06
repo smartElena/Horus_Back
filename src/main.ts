4795074aed0f3bb1b8d74b21c557b4602a2eda51
GalvisDev <galvisdeveloper@gmail.com>
2024-06-05 20:05:21 -0500

    Prueba cors sin configuraciones

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const allowedOrigins = [
     'https://web-smartsystems.com.co/',
     'http://localhost:5173',
   ];
   // app.setGlobalPrefix('api');
-  app.enableCors();
+  // app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
