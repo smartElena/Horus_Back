0c550e5e1e2d45936e6ea82b331118d0635cd413
GalvisDev <galvisdeveloper@gmail.com>
2024-06-05 20:01:37 -0500

    Prueba cors sin configuraciones

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,9 @@ async function bootstrap() {
   const allowedOrigins = [
     'https://web-smartsystems.com.co/',
     'http://localhost:5173',
-    // Añade otros dominios que necesites permitir
   ];
   // app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: (origin, callback) => {
-      // Permitir solicitudes sin origen (como las hechas por Postman o cURL)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('No permitido por CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
